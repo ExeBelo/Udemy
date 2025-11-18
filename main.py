c3220b330931d97181e75cc4334c02e997df57c5
Exebelo <lautarobelo@gmail.com>
2025-11-17 21:32:38 -0300

    test app firebase

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-from fastapi import FastApi
+from fastapi import FastAPI
+import firebase_admin
+from firebase_admin import credentials, firestore
 
-app = FastApi(debug=True, title="Udemy FastAPI", version="0.0.0")
+app = FastAPI(debug=True, title="Udemy FastAPI", version="0.0.0")
+
+cred = credentials.Certificate("key.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+@app.get("/usuarios")
+def get_usuarios():
+    usuarios = db.collection("usuarios")
+    lista_usuarios = [doc.to_dict() for doc in usuarios.stream()]
+    return lista_usuarios
+@app.get("/cursos")
+def get_cursos():
+    cursos = db.collection("cursos")
+    lista_cursos = [doc.to_dict() for doc in cursos.stream()]
+    return lista_cursos
+ 
